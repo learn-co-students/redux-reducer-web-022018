36dab85f909c7b05ed8161097ccb464ddfcb33a7
sinclairstudy <sinclairstudy@gmail.com>
2018-05-09 16:51:51 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+  switch (action.type){
+    case 'ADD_FRIEND':
+      if (action.friend){
+        let newFriend = action.friend
+        if (newFriend.name && newFriend.hometown && newFriend.id){
+          return {...state, friends: [...state.friends, newFriend]}
+        }
+      }
+      break;
+    case 'REMOVE_FRIEND':
+      if (action.id){
+        return {...state, friends: state.friends.filter(x => x.id !== action.id)}
+      }
+      break;
+  }
+  return state;
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+  switch(action.type){
+    case 'INCREASE':
+      return {...state, numberOfPresents: state.numberOfPresents + 1}
+      break
+    case 'DECREASE':
+      return {...state, numberOfPresents: state.numberOfPresents - 1}
+      break
+  }
+  return state
 }
